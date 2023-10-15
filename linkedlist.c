3468170a1b1415b186a6454378ebc7776bbe1d79
tumi-thaki <tumiethaki01@gmail.com>
2023-10-15 01:18:23 -0700

    manipulation of the alias_t and list_t: addition and free memory

---
+++ linkedlist.c
@@ -0,0 +1,118 @@
+#include "shell.h"
+
+alias_t *add_alias_end(alias_t **head, char *name, char *value);
+void free_alias_list(alias_t *head);
+list_t *add_node_end(list_t **head, char *dir);
+void free_list(list_t *head);
+
+/**
+ * add_alias_end - adds a node to the end of a alias_t linked list
+ * @head: pointer to the head of the list_t list
+ * @name: name of the new alias to be added
+ * @value: value of the new alias to be added
+ *
+ * Return: a pointer to the new node, NULL for failure
+*/
+
+alias_t *add_alias_end(alias_t **head, char *name, char *value)
+{
+	alias_t *new_node = malloc(sizeof(alias_t));
+	alias_t *last;
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->next = NULL;
+
+	new_node->name = malloc(sizeof(char) * (_strlen(name) + 1));
+
+	if (!new_node->name)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = value;
+	_strcpy(new_node->name, name);
+
+	if (*head)
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new_node;
+	}
+	else
+		*head = new_node;
+
+	return (new_node);
+}
+
+/**
+ * add_node_end - adds a node to the tail of a list_t linked list
+ * @head: pointer to the head of the list_t list
+ * @dir: directory path for the new node
+ *
+ * Return: a pointer to the new node, NULL for failure
+*/
+
+list_t *add_node_end(list_t **head, char *dir)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *last;
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->dir = dir;
+	new_node->next = NULL;
+
+	if (*head)
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new_node;
+	}
+	else
+		*head = new_node;
+
+	return (new_node);
+}
+
+/**
+ * free_alias_list - frees a alias_t linked list
+ * @head: head of the alias_t list
+*/
+
+void free_alias_list(alias_t *head)
+{
+	alias_t *next;
+
+	while (head)
+	{
+		next = head->next;
+		free(head->name);
+		free(head->value);
+		free(head);
+		head = next;
+	}
+}
+
+/**
+ * free_list - Frees a list_t linked list.
+ * @head: The head of the list_t list.
+*/
+
+void free_list(list_t *head)
+{
+	list_t *next;
+
+	while (head)
+	{
+		next = head->next;
+		free(head->dir);
+		free(head);
+		head = next;
+	}
+}
